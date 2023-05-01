0d297f341993ec565ba00ca353c6f61ff8a6fb4a
Admin <abhihkakadiya2301@gmail.com>
2023-05-01 17:16:48 +0530

    JWT Token authentication

--- venv/routers/auth.py
+++ venv/routers/auth.py
@@ -2,12 +2,12 @@ import schemas
 from fastapi import APIRouter,Depends,HTTPException,status,Response
 from sqlalchemy.orm import Session
 import database,schemas,model,utils
-from database import get_db
+from database import *
 
-router = APIRouter(tags=["Authentication"])
+router = APIRouter(tags=['authentication'])
 @router.post("/login")
-def login(user_cred : schemas.UserLogin,db : Session = Depends(database.get_db())):
-    user = db.query(model.User).filter(model.User.email == User_cred.email).first
+def login(user_cred : schemas.UserLogin,db:Session = Depends(get_db)):
+    user = db.query(model.User).filter(model.User.email == user_cred.email).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Credintials")
     if not utils.verify(user_cred.password,user.password):
